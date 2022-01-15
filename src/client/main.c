32e765ee2dfdbf9f9e0d25c3538042b8cb8b6381
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-15 14:49:28 -0300

    add logs

--- src/client/main.c
+++ src/client/main.c
@@ -1,8 +1,8 @@
+#include <getopt.h>
+#include <unistd.h>
+
 #include "client.h"
 #include "../includes/utils.h"
-
-#include <unistd.h>
-#include <getopt.h>
 
 int main(int argc, char *argv[])
 {
@@ -10,33 +10,32 @@ int main(int argc, char *argv[])
         char *server_port = "";
         char *username = "";
 
-        char optc = 0;
-
         struct option LongOptions[] = {
                 {"address", required_argument, NULL, 'a'},
                 {"port", required_argument, NULL, 'p'},
                 {"username", required_argument, NULL, 'u'}
         };
 
+        char optc = 0;
         while ((optc = getopt_long(argc, argv, "a:p:u:", LongOptions, NULL)) != -1) {
                 switch (optc) {
                 case 'a':
                         if (!validate_ipv4(optarg)) {
-                                fprintf(stderr, "invalid ip address\n");
+                                LOG_ERROR("invalid ip address %s", optarg);
                                 exit(EXIT_FAILURE);
                         }
                         server_ip = optarg;
                         break;
                 case 'p':
                         if (!validate_port(optarg)) {
-                                fprintf(stderr, "invalid port\n");
+                                LOG_ERROR("invalid port %s", optarg);
                                 exit(EXIT_FAILURE);
                         }
                         server_port = optarg;
                         break;
                 case 'u':
                         if (!validate_username(optarg)) {
-                                fprintf(stderr, "invalid username\n");
+                                LOG_ERROR("invalid username %s", optarg);
                                 exit(EXIT_FAILURE);
                         }
                         username = optarg;
@@ -50,7 +49,7 @@ int main(int argc, char *argv[])
             !validate_port(server_port) ||
             !validate_username(username)) {
 
-                fprintf(stderr, "usage: %s -a ip -p port -u username\n", argv[0]);
+                LOG_ERROR("usage: %s -a ip -p port -u username", argv[0]);
                 exit(EXIT_FAILURE);
         }
 
